91378947b3d684ec67e1574e5fbf6c185682504b
VietAnh <vietanhtb057@gmail.com>
2026-03-11 09:18:33 +0700

    Clone, cd and change branch must be performed manually

--- run_template.sh
+++ run_template.sh
@@ -6,7 +6,6 @@ set -e
 echo "=== SETTING UP EXPERIMENT ==="
 
 # --- IMPORTANT PATHS ---
-WORKING_BRANCH="hp_search"
 TEMPLATE_PATH="configs/baseline/training_configs.yaml.template"
 CONFIG_PATH="configs/baseline/training_configs.yaml"
 SCRIPT="scripts/baseline/qdora_vit5/train.py"                       
@@ -15,28 +14,21 @@ OUTPUT_DIR="model_adapter"
 
 export LEARNING_RATE=0.0003143144372398535
 
-# clone repo
-git clone https://github.com/VietAnh-AI2-UET/ViT5_Base_QLoRa_VNDS.git
-
-# Move to working directory
-cd ViT5_Base_QLoRa_VNDS
-
-# checkout current working branch
-git checkout $WORKING_BRANCH
-
 # install gettext for bash
+echo "installing gettext library for bash"
 apt-get install gettext
 
 # install requirements.txt
-pip install requirements.txt
+echo "installing requirements.txt"
+pip install -qq -r requirements.txt
 
 # Sinh file cấu hình hoàn chỉnh từ file yaml template
-echo "1. Creating configs file at: $CONFIG_PATH"
+echo "Creating configs file at: $CONFIG_PATH"
 
 (echo "cat <<EOF"; cat $TEMPLATE_PATH; echo ""; echo "EOF") | bash > $CONFIG_PATH
 
 # Bắt đầu chạy file
-echo "2. Start running script"
+echo "Start running script"
 
 python $SCRIPT --config $CONFIG_PATH --output_dir $OUTPUT_DIR
 
