0001103480d77ba3cec300b1eae1d9f00f298d0b
VietAnh <vietanhtb057@gmail.com>
2026-03-17 08:31:51 +0700

    Upload hp_search script for custom QLORA exp

---
+++ scripts/custom_qlora/hp_search.py
@@ -0,0 +1,254 @@
+import os
+import shutil
+import yaml
+import argparse
+import torch
+import numpy as np
+from datasets import load_dataset
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForSeq2SeqLM, 
+    BitsAndBytesConfig,
+    Seq2SeqTrainingArguments, 
+    Seq2SeqTrainer, 
+    DataCollatorForSeq2Seq,
+    EarlyStoppingCallback
+)
+from peft import get_peft_model, prepare_model_for_kbit_training, LoraConfig
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Fine-tune ViT5 using QDoRa/QLoRA for Text Summarization")
+    
+    parser.add_argument(
+        "--configs", 
+        type=str,
+        required=True,
+        help="Enter YAML training configs file path"
+    )
+
+    parser.add_argument(
+        "--n_trials",
+        type=int,
+        required=False,
+        default=5,
+        help="Number of trials for Optuna hyperparameter search"
+    )
+
+    parser.add_argument(
+        "--use_dora",
+        type=bool,
+        required=True,
+        help="Which method do you want to use? DORA --> True / LORA --> False"
+    )
+
+    parser.add_argument(
+        "--checkpoint_dir",
+        type=str,
+        required=False,
+        default="model_checkpoint",
+        help="Where do you want to save the model's checkpoint?"
+    )
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    print("===== RUNNING CUSTOMIZE_RANK.PY =====")
+
+    # 0. Read YAML Configs
+    with open(args.configs, 'r', encoding='utf-8') as file:
+        configs = yaml.safe_load(file)
+
+    # --- TRAINING PARAMETERS ---
+    MODEL_NAME = configs["model"]["model_name"]
+    DATASET_NAME = configs["model"]["dataset_name"]
+
+    TRAIN_SAMPLES = configs["data"]["train_samples"]
+    VAL_SAMPLES = configs["data"]["val_samples"]
+
+    # LoRa configs mặc định (dùng làm fallback)
+    LORA_R = configs["lora"]["lora_r"]
+    LORA_ALPHA = configs["lora"]["lora_alpha"]
+    LORA_TARGET_MODULE = configs["lora"]["lora_target_module"]
+    LORA_DROPOUT = configs["lora"]["lora_dropout"]
+    USE_DORA = args.use_dora
+
+    EPOCHS = configs["training"]["epochs"]
+    # LR = configs["training"]["learning_rate"]
+    WEIGHT_DECAY = configs["training"]["weight_decay"]
+    WARMUP_RATIO = configs["training"]["warmup_ratio"]
+    LABEL_SMOOTHING_FACTOR = configs["training"]["label_smoothing_factor"]
+    BATCH_SIZE = configs['training']['batch_size']
+    GRAD_ACCUM_STEPS = configs['training']['gradient_accumulation_steps']
+    N_TRIALS=args.n_trials
+
+    # OUTPUT DIRECTORY
+    CHECKPOINT_DIR = args.checkpoint_dir
+
+    print(f"Step 0: Loading configuration for {MODEL_NAME} fine-tuning completed")
+
+    # 1. Load Tokenizer & Dataset
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    dataset = load_dataset(DATASET_NAME)
+
+    dataset["train"] = dataset["train"].shuffle(seed=42).select(range(TRAIN_SAMPLES))
+    dataset["validation"] = dataset["validation"].shuffle(seed=42).select(range(VAL_SAMPLES))
+
+    print("STEP 1: LOADING TOKENIZER AND DATASET COMPLETED")
+    
+    # 2. Preprocess Data
+    def preprocess_function(batch):
+        input_texts = ['tóm tắt: ' + txt for txt in batch['article']]
+        input_max_length = 1024
+        target_max_length = 256
+
+        model_inputs = tokenizer(input_texts, max_length=input_max_length, truncation=True)
+        labels = tokenizer(text_target=batch['abstract'], max_length=target_max_length, truncation=True)
+        model_inputs['labels'] = labels['input_ids']
+        return model_inputs
+
+    tokenized_dataset = dataset.map(preprocess_function, batched=True)
+    
+    print("STEP 2: PREPROCESSING DATA COMPLETED")
+
+    # 3. Setup QLORA/QDORA Model
+    def model_init():
+        """Return a brand new model for each trial"""
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_quant_type='nf4',
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True
+        )
+
+        base_model = AutoModelForSeq2SeqLM.from_pretrained(
+            MODEL_NAME,
+            quantization_config=bnb_config,
+            device_map='auto'
+        )
+        base_model = prepare_model_for_kbit_training(base_model, use_gradient_checkpointing=False)
+        
+        rank_pattern = {}
+        alpha_pattern = {}
+
+        num_layers = 12
+        for i in range(num_layers):
+            if i < 4:
+                r, alpha = 4, 8
+            elif i < 8:
+                r, alpha = 8, 16
+            else:
+                r, alpha = 16, 32
+
+            rank_pattern[f"encoder.block.{i}"] = r
+            rank_pattern[f"decoder.block.{i}"] = r
+
+            alpha_pattern[f"encoder.block.{i}"] = alpha
+            alpha_pattern[f"decoder.block.{i}"] = alpha
+
+        lora_config_kwargs = {
+            "r": LORA_R,
+            "lora_alpha": LORA_ALPHA,
+            "target_modules": LORA_TARGET_MODULE,
+            "lora_dropout": LORA_DROPOUT,
+            "bias": "none",
+            "task_type": "SEQ_2_SEQ_LM",
+            "use_dora": USE_DORA
+        }
+
+        lora_config_kwargs["rank_pattern"] = rank_pattern
+        lora_config_kwargs["alpha_pattern"] = alpha_pattern
+
+        lora_config = LoraConfig(**lora_config_kwargs)
+        
+        model = get_peft_model(base_model, lora_config)
+        return model
+
+    print(f"STEP 3: SETTING UP {MODEL_NAME} QUANTIZATION COMPLETED")
+
+    def optuna_hp_space(trial):
+        """
+        --Hyperparameters search space--
+        Define which parameter you want to perform search with Optuna
+        """
+        return {
+            "learning_rate": trial.suggest_float("learning_rate", 1e-5, 5e-4, log=True),
+        }
+    
+    def compute_objective(metrics):
+        """
+        Return the target that you want to optimize (currently validation loss)
+        """
+        return metrics["eval_loss"]
+
+    # 4. Training Arguments
+    training_args = Seq2SeqTrainingArguments(
+        output_dir=CHECKPOINT_DIR,
+        num_train_epochs=EPOCHS,
+        # learning_rate=LR,
+        lr_scheduler_type="cosine",
+        weight_decay=WEIGHT_DECAY,
+        warmup_ratio=WARMUP_RATIO,
+        label_smoothing_factor=LABEL_SMOOTHING_FACTOR,
+        per_device_train_batch_size=BATCH_SIZE,
+        gradient_accumulation_steps=GRAD_ACCUM_STEPS,
+        per_device_eval_batch_size=BATCH_SIZE * 2,
+        optim="adamw_torch",
+        fp16=True,
+        logging_steps=25,
+        eval_strategy="epoch",
+        save_strategy="epoch",
+        save_total_limit=2,
+        load_best_model_at_end=True,
+        metric_for_best_model="eval_loss",
+        greater_is_better=False,
+        predict_with_generate=False,
+        report_to="tensorboard",
+        seed=42
+    )
+
+    dummy_model_for_data_collator = model_init()
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer, 
+        model=dummy_model_for_data_collator)
+    
+    early_stopping_callback = EarlyStoppingCallback(early_stopping_patience=5, early_stopping_threshold=0.0)
+    
+    print("STEP 4: SETTING UP HYPERPARAMS COMPLETED")
+
+    # 5. Trainer
+    trainer = Seq2SeqTrainer(
+        model=model_init,
+        args=training_args,
+        train_dataset=tokenized_dataset['train'],
+        eval_dataset=tokenized_dataset['validation'],
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        callbacks=[early_stopping_callback]
+    )
+
+    # Start Training
+    print(f"===== START HYPERPARAMS SEARCHING {MODEL_NAME} =====")
+    best_trial = trainer.hyperparameter_search(
+        direction="minimize",
+        backend="optuna",
+        hp_space=optuna_hp_space,
+        compute_objective=compute_objective,
+        n_trials=N_TRIALS
+    )
+
+    print("="*50)
+    print("Hyperparameters searching completed!")
+    print(f"Best hp found: {best_trial.hyperparameters}")
+    print(f"Best eval_loss: {best_trial.objective}")
+    print("="*50)
+
+    # Create archive file for model's checkpoints
+    shutil.make_archive(CHECKPOINT_DIR, "zip", CHECKPOINT_DIR)
+
+    print(f"Thư mục '{CHECKPOINT_DIR}' đã được nén thành công thành '{CHECKPOINT_DIR}.zip'")
+    print(f"Bạn có thể tải tệp '{CHECKPOINT_DIR}.zip' từ phần duyệt tệp của Colab (biểu tượng thư mục ở bên trái).")
+
+if __name__ == "__main__":
+    main()
