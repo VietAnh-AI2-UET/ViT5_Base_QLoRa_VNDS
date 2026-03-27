52f6c67792f6148b1b94248810ddab2d382e5365
VietAnh <vietanhtb057@gmail.com>
2026-03-27 12:43:54 +0700

    Fix zipping bug

--- scripts/utils/save_utils.py
+++ scripts/utils/save_utils.py
@@ -6,8 +6,8 @@ def save_model(trainer, tokenizer, adapter_dir, checkpoint_dir):
     trainer.save_model(adapter_dir)
     tokenizer.save_pretrained(adapter_dir)
 
-    shutil.make_archive(adapter_dir, zip, adapter_dir)
+    shutil.make_archive(adapter_dir, "zip", adapter_dir)
     print(f"Thư mục '{adapter_dir}' đã được nén thành '{adapter_dir}.zip'")
 
-    shutil.make_archive(checkpoint_dir, zip, checkpoint_dir)
+    shutil.make_archive(checkpoint_dir, "zip", checkpoint_dir)
     print(f"Thư mục '{checkpoint_dir}' đã được nén thành '{checkpoint_dir}.zip'")
